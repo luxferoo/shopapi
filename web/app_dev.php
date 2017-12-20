90bbe957d1d942895fe495ccae73a7a222aa729b
luxfero <imamharir@gmail.com>
2017-12-20 23:07:31 +0000

    PATCH method allowed (TEMPORARY SOLUTION )

--- web/app_dev.php
+++ web/app_dev.php
@@ -16,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
         // may also be using PUT, PATCH, HEAD etc
-        header("Access-Control-Allow-Methods: GET, POST, PUT , OPTIONS");
+        header("Access-Control-Allow-Methods: GET, POST, PUT , PATCH , OPTIONS");
 
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
         header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
